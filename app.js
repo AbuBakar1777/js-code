ebe6f35525d83abc63cfdcaf4ae98c14a274f816
AbuBakar1777 <74306250+AbuBakar1777@users.noreply.github.com>
2020-12-30 03:06:28 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,131 @@
+// alert("Asslicum Two class");
+// console.log("helloew");
+// var a=5;
+// var b=3;
+// var c=a+b;
+// alert(c);
+
+// var f=10;
+// var c=f++ + ++f - f + f++ - ++f + f;
+// // var c="12";
+// // var s=f*c;
+// alert(c);
+// for(var a=1; a<=100; a=a+10){
+//     for( var b=a;b<a+10;b++){
+// document.write(b+" ");
+// }
+
+// document.write("<br>")
+// }
+// casing class
+// 
+// var city=prompt('enter your name')
+// city=city.toLowerCase();
+// var arr=['karachi','Lahore','islamabad']
+// for(var i=0;i<arr.length;i++){
+//     if(arr[i]===city){
+//         alert('City was found')
+//     }
+//     else{
+//         alert('city was not found')
+// }}
+
+// var city=prompt('enter your name')
+// var firstchar=city.slice(0,1);
+// var otherchar =city.slice(1);
+// firstchar=firstchar.toUpperCase();
+// otherchar=otherchar.toLowerCase();
+// var city=firstchar+otherchar;
+// console.log(city);
+
+// var arr=['karachi','Lahore','islamabad']
+// for(var i=0;i<arr.length;i++){
+//     if(arr[i]===city){
+//         alert('City was found')
+//     }
+//     else{
+//         alert('city was not found')
+// }}
+
+    
+// 
+// var str = prompt("Enter some text"); 2 var numChars = str.length; 3 for (var i = 0; i < numChars; i++) { 4   if (str.slice(i, i + 2) === " ") { 5     alert("No double spaces!"); 6     break
+// var text='It is startling to think that even in the darkest depths of World War II, J. R. R. Tolkien was writing the trilogy, which contains, with the weird applicability available only to poetry and myth, the essential notion that the good gray wizard can understand the evil magi precisely because he is just enough like them to grasp their minds and motives in ways that they cannot grasp his. '
+// for(var i=0;i<text.length;i++){
+//     if(text.slice (i,i+12)==="It is startl")
+//     {text = text.slice(0, 1) + "the Second World War" + text.slice(i + 12)
+//         console.log(text)
+//     }
+// }
+//Finding a string location
+//  var a='ghous';
+// console.log(a.charAt(1))
+// var para='this is example example';
+// var para1=para.replace("example","text");
+// console.log(para1)
+// var para='this is example example';
+// var para1=para.replace("/example/g","text");
+// console.log(para1)
+// finding random number
+// var headuse =prompt ("enter head name");
+// var tailuser =prompt("enter tail name");
+// var toss=Math.random()*2;
+// var floor=Math.floor(toss);
+// if(floor==0){
+//     alert("head"+headuse+"win the toss")
+// }
+// else{
+//     alert("tail"+tailuser+"win the toss")
+// }
+// var a=new Date();
+// document.write(a);
+
+// var days=["mond,tuesd,wednesd,thursday,friday,saturaday,sunday"]
+// var now=new Date();
+
+// var dob=new Date( prompt("enter your age","Jan 12,2020"));
+// var dobmili=dob.getTime();
+// var today=new Date();
+// var todmili=today.getTime();
+// var diff=todmili-dobmili;
+// var accurate=Math.floor(diff/(1000*60*60*24*30*12));
+// document.write(accurate);
+
+
+// Function
+
+// function add(a,b,c){
+//     var z=(a+b+c);
+// }
+// var z=add(12,33,77);
+// alert(z);
+// function agecalc(){
+// var dob=new Date( prompt("enter your age","Jan 12,2020"));
+// var dobmili=dob.getTime();
+// var today=new Date();
+// var todmili=today.getTime();
+// var diff=todmili-dobmili;
+// var accurate=Math.floor(diff/(1000*60*60*24*30*12));
+// document.write(accurate);
+// }
+// agecalc();
+// function first(a,b){
+//     return "hellow";
+   
+// }
+// console.log(first(2,4))
+// function cal(a,oper,b){
+// if(oper==="+"){
+// return a+b;}
+// else if(oper==="-"){
+//     return a-b;
+// }
+// else if(oper==="*"){
+//     return a*b;
+// }else{
+//     return "incorrect";
+// }
+
+// }
+// // 
+
